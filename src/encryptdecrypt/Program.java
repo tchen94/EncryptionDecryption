40c32debdb7f539fc569741ccdf8d48514e44c1c
Tim Chen <tchen994@gmail.com>
2021-05-12 18:27:18 -0400

    Formatted for easier browsing

--- src/encryptdecrypt/Program.java
+++ src/encryptdecrypt/Program.java
@@ -4,7 +4,9 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Encryption protect = new Encryption();
+        // Atbash Cipher
         String phrase = "we found a treasure!";
+        System.out.println(protect.atbashCipher(phrase));
 
         System.out.println(protect.reverseABCEncryption(phrase));
     }
