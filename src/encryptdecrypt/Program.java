100621cacb04af14bcf3e4f0092f9569d211483c
Tim Chen <tchen994@gmail.com>
2021-05-12 18:26:46 -0400

    Imported for user input

--- src/encryptdecrypt/Program.java
+++ src/encryptdecrypt/Program.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Encryption protect = new Encryption();
         String phrase = "we found a treasure!";
 
