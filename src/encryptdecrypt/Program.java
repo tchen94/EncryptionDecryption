f29c5e9ca3a200f0b5d83080f7075056b8200a19
Tim Chen <tchen994@gmail.com>
2021-05-12 23:18:18 -0400

    Moved to Encryption class

--- src/encryptdecrypt/Program.java
+++ src/encryptdecrypt/Program.java
@@ -4,13 +4,6 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Encryption protect = new Encryption();
-        // Atbash Cipher
-        String phrase = "we found a treasure!";
-        System.out.println(protect.atbashCipher(phrase));
-
-        // Caesar Cipher
-        String phraseInput = scanner.nextLine();
-        int numInput = Integer.parseInt(scanner.nextLine());
-        System.out.println(protect.caesarCipher(phraseInput, numInput));
+        protect.start();
     }
 }
